3dd8c36199151c60f9a32f9753fed1272914365a
the-abra <aegean.sword1@gmail.com>
2024-10-09 19:18:39 +0300

    aa

--- .setup/depends.sh
+++ .setup/depends.sh
@@ -2,12 +2,13 @@
 
 # List of packages to check
 packages=("python3" "python3-flask" "wget" "net-tools" "openssh-client" "curl" "python3-pip" "nginx" "ncurses")
-
+aa=true
 # Loop through each package
 for pkg in "${packages[@]}"
 do
     # Check if package is installed
     if ! (dpkg -l | grep -q "^ii  $pkg "); then
+        [[ $aa == true ]] && apt update && aa=false
         apt -qq install $pkg -yq # silent mode
         # not need nginx at this project for now
         # [[ $pkg == "nginx" ]] && soruce .setup/nginx.sh
