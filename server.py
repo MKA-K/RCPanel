5e82f6a64905f6a395893ab0287d061ad6bbf5b1
the-abra <aegean.sword1@gmail.com>
2024-10-11 16:41:05 +0300

    aa

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
@@ -15,6 +15,10 @@ def notify():
     title = request.args.get('title', 'INFO')
     description = request.args.get('description', 'Empty description')
     return render_template('notification.html', title=title, description=description)
+
+@app.route('/sound/<path:filename>')
+def serve_sound(filename):
+    return send_from_directory('static/sounds', filename)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.getElementById("loading").style.display = "none";
+        var audio = new Audio('/sound/expand.wav');
+        audio.play();
     }, 2000); 
     setTimeout(function() {
         document.getElementById("notifyiframe").src = "/notify?title=Hello!&description=Welcome to MKA-KΣ RC Panel"; 
