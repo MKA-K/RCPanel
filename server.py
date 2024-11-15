bbf69620e1bbedad4f64c840b578de25a3dd17b3
the-abra <aegean.sword1@gmail.com>
2024-11-15 19:02:52 +0300

    aa

--- server.py
+++ server.py
@@ -69,4 +69,8 @@ def receive_data():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(
+    debug=bool(config.get('runtime', 'debug')),
+    host=config.get('runtime', 'host'),
+    port=int(config.get('runtime', 'port'))
+)
